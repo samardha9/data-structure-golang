fd0285531b4b647676c85a7facefa6445f90c250
Samardha <samar@Samardhas-MacBook-Air.local>
2023-11-28 13:22:26 -0500

    implemented modules

---
+++ channels.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func DemonstrateChannels() {
+	ch := make(chan string)
+
+	go func() {
+		time.Sleep(2 * time.Second)
+		ch <- "Message from channel!"
+	}()
+
+	fmt.Println("Channel demonstration:")
+	msg := <-ch // Receive message from channel
+	fmt.Println(msg)
+}
---
+++ strings.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func DemonstrateStrings() {
+	str := "Hello, Go!" // Declare a string
+
+	fmt.Println("String demonstration:")
+	fmt.Printf("String: %s\n", str)
+	fmt.Printf("Length: %d\n", len(str))
+	fmt.Printf("Character at index 0: %c\n", str[0])
+}
